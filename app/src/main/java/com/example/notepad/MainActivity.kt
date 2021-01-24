9a33cdf6965d7ab8908f9f56828da8dddbc1c48b
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2021-01-25 00:04:37 +0900

    Initial commit

---
+++ app/src/main/java/com/example/notepad/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.notepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // 코드 실행문
+}
